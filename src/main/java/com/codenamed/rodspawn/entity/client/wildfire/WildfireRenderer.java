6c99f25c854c961ca58d7af64399b85db4baef39
Super1166 <110970817+Super1166@users.noreply.github.com>
2025-02-14 14:30:27 +0200

    Tweaks

--- src/main/java/com/codenamed/rodspawn/entity/client/wildfire/WildfireRenderer.java
+++ src/main/java/com/codenamed/rodspawn/entity/client/wildfire/WildfireRenderer.java
@@ -7,7 +7,9 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.monster.Blaze;
 
 public class WildfireRenderer extends MobRenderer<Wildfire, WildfireModel> {
     public WildfireRenderer(EntityRendererProvider.Context context) {
@@ -23,4 +25,9 @@ public class WildfireRenderer extends MobRenderer<Wildfire, WildfireModel> {
     public void render(Wildfire entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight) {
         super.render(entity, entityYaw, partialTicks, poseStack, buffer, packedLight);
     }
+
+    protected int getBlockLightLevel(Wildfire entity, BlockPos pos) {
+        return 15;
+    }
+
 }
